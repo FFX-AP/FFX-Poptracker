2bd6ce569cdd8c63d1f2d0bec00a4b5d4a9a238b
Andrew K <andrewkilbourn1996@gmail.com>
2025-11-25 14:50:12 +1100

    Kilika + Aeons

--- scripts/logic/core.lua
+++ scripts/logic/core.lua
@@ -2,11 +2,3 @@
 function has(item)
     return Tracker:ProviderCountForCode(item) > 0
 end
-
-function canReachAnima()
-    if (hasBesaid() and hasKilika() and hasDjose() and hasMacalania() and hasZanarkand()) then
-        return true
-    else
-        return false
-    end
-end
