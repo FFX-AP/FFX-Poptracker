ee7bcf92efffd81865d8345f56a9ab48ff46a170
Andrew K <andrewkilbourn1996@gmail.com>
2026-01-21 10:58:17 +1100

    Capture Tracking Key Update

--- scripts/autotracking/capture_tracking.lua
+++ scripts/autotracking/capture_tracking.lua
@@ -1,6 +1,7 @@
 -- key = Slot:1:FFX_CAPTURE:54
 function updateCaptures(key, value, oldValue)
-    local arena_idx = tonumber(string.match(string.match(string.match(key, ":(.*)"), ":(.*)"), ":(.*)"))
+    local arena_idx = string.match(key, "[^:]*:[^:]*:.*_(.*)")
+    -- local arena_idx = tonumber(string.match(string.match(string.match(key, ":(.*)"), ":(.*)"), ":(.*)"))
     local fiend = Tracker:FindObjectForCode(AllCaptures[arena_idx])
     
     if fiend ~= nil and (value ~= 0 or oldValue ~= 0) then
